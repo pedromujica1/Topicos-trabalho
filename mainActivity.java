53e3b013704351f914b692d49e4cf1078f655df1
Pedro Miotto Mujica <107735941+pedromujica1@users.noreply.github.com>
2023-04-14 15:59:21 -0300

    Update mainActivity.java

--- mainActivity.java
+++ mainActivity.java
@@ -1,5 +1,4 @@
-//https://blogger.googleusercontent.com/img/a/AVvXsEifgvU8-xQ-GVGEZ0N2Rm7-xFRFiG6lqgdh_MTG0ddKhchQRpFHTOKcjP1dH_NuwQGI7ZGCnVKf16guMvNOemmW1stUtJNF2l4PlxzInP8Agodmm0qvQ5DILYqXaPYvUCp2sPg1kQ1AY7rC29gEYFO1IZTq5nNyOuXSEU0wVyHBa5u4D0Wdx0GkJAWV_Q=w1200-h630-p-k-no-nu
-package com.example.jokepo;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,81 +10,115 @@ import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-    private TextView name1;
-    private TextView name2;
-    private TextView textResult;
-
+    public TextView jogador1;
+    public TextView jogador2;
+    public TextView textResult;
     public ImageView papelJog1;
+    public ImageView papelJog2;
+    public ImageView pedraJog1;
+    public ImageView pedraJog2;
+    public ImageView tesouraJog1;
+    public ImageView tesouraJog2;
 
-
+    public TextView resultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        name1 = findViewById(R.id.textResultado);
-        name2 = findViewById(R.id.textResultado);
+        jogador1 = findViewById(R.id.textJogador1);
+        jogador2 = findViewById(R.id.textJogador2);
         textResult = findViewById(R.id.textResultado);
+        papelJog1 = findViewById(R.id.papelJog1);
+        papelJog2 = findViewById(R.id.papelJog2);
 
+        pedraJog1 = findViewById(R.id.pedraJog);
+        pedraJog2 = findViewById(R.id.pedraJog2);
+
+        tesouraJog1 = findViewById(R.id.tesouraJog1);
+        tesouraJog2 = findViewById(R.id.tesouraJog2);
+
+        resultado = findViewById(R.id.textResultado);
     }
     public void jogar (View View){
 
         Button button =findViewById(R.id.button);
+
         Random random = new Random();
+
+        String nome1 = jogador1.getText().toString();
+        String nome2 = jogador2.getText().toString();
 
         int numAleatorioJog1 = random.nextInt(3);
         int numAleatorioJog2 = random.nextInt(3);
 
-        ImageView papelJog1 = findViewById(R.id.papelJog1);
-        ImageView papelJog2 = findViewById(R.id.papelJog2);
 
-        ImageView pedraJog1 = findViewById(R.id.pedraJog1);
-        ImageView pedraJog2 = findViewById(R.id.pedraJog2);
 
-        ImageView tesouraJog1 = findViewById(R.id.tesouraJog1);
-        ImageView tesouraJog2 = findViewById(R.id.tesouraJog2);
-
-        ImageView imageView = null;
-        if (numAleatorioJog1 == 1) {
+        if (numAleatorioJog1 == 0) {
             papelJog1.setVisibility(View.VISIBLE);
-//            pedraJog1.setImageDrawable(null);
-//            tesouraJog1.setImageDrawable(null);
-        } else if (numAleatorioJog1 == 2) {
-            pedraJog1.setVisibility(View.VISIBLE);
-//            papelJog1.setImageDrawable(null);
-//            tesouraJog1.setImageDrawable(null);
-        } else {
-            tesouraJog1.setVisibility(View.VISIBLE);
-//            papelJog1.setImageDrawable(null);
-//            pedraJog1.setImageDrawable(null);
-        }
+            pedraJog1.setVisibility(View.INVISIBLE);
+            tesouraJog1.setVisibility(View.INVISIBLE);
 
-        if (numAleatorioJog2 == 1) {
-            papelJog2.setVisibility(View.VISIBLE);
-
-//            pedraJog2.setImageDrawable(null);
-//            tesouraJog2.setImageDrawable(null);
-        } else if (numAleatorioJog2 == 2) {
-            pedraJog2.setVisibility(View.VISIBLE);
-//            imageView.setImageResource(R.drawable.pedra);
 //
-//            papelJog2.setImageDrawable(null);
-//            tesouraJog2.setImageDrawable(null);
-
-        } else {
-            tesouraJog2.setVisibility(View.VISIBLE);
-            //imageView.setImageResource(R.drawable.tesoura);
-
-            //papelJog2.setImageDrawable(null);
-            //pedraJog2.setImageDrawable(null);
+        } else if (numAleatorioJog1 == 1) {
+            pedraJog1.setVisibility(View.VISIBLE);
+            tesouraJog1.setVisibility(View.INVISIBLE);
+            papelJog1.setVisibility(View.INVISIBLE);
+//
+        } else if (numAleatorioJog1 == 2) {
+            tesouraJog1.setVisibility(View.VISIBLE);
+            papelJog1.setVisibility(View.INVISIBLE);
+            pedraJog1.setVisibility(View.INVISIBLE);
+//
         }
-//        Pedra perde para papel[1] e ganha para tesoura[3]
-//        Papel perde para tesoura[3] e ganha para pedra[2]
-//        Tesoura perde para pedra[2] e ganha para papel[1]
-        //int[] images = {R.drawable.papel, R.drawable.tesoura, R.drawable.pedra};
-        if(((numAleatorioJog1 == 3) && (numAleatorioJog2 == 1)) || ((numAleatorioJog1 == 2) && (numAleatorioJog2 == 3)) || ((numAleatorioJog1 == 1) && (numAleatorioJog2 == 2))){
-            textResult = name1;
-        }else if(numAleatorioJog1 != numAleatorioJog2) {
-            textResult = name2;
+
+        if (numAleatorioJog2 == 0) {
+            papelJog2.setVisibility(View.VISIBLE);
+            pedraJog2.setVisibility(View.INVISIBLE);
+            tesouraJog2.setVisibility(View.INVISIBLE);
+//
+        } else if (numAleatorioJog2 == 1) {
+            pedraJog2.setVisibility(View.VISIBLE);
+            tesouraJog2.setVisibility(View.INVISIBLE);
+            papelJog2.setVisibility(View.INVISIBLE);
+//
+        } else if (numAleatorioJog2 == 2){
+            tesouraJog2.setVisibility(View.VISIBLE);
+            pedraJog2.setVisibility(View.INVISIBLE);
+            papelJog2.setVisibility(View.INVISIBLE);
+        }
+
+        //Empate
+        if (numAleatorioJog1 == numAleatorioJog2){
+            resultado.setText("Empate! Jogue de novo");
+        }
+
+
+
+        //Jogador 1
+        //(numAleatorioJog1 == 2) && (numAleatorioJog2 == 0)) tesoura ganha do papel
+        //((numAleatorioJog1 == 0) && (numAleatorioJog2 == 1)) papel ganha da pedra
+        //((numAleatorioJog1 == 1) && (numAleatorioJog2 == 2) pedra ganha da tesoura
+
+        if((numAleatorioJog1 == 2) && (numAleatorioJog2 == 0)){
+            resultado.setText("O vencedor é o jogador 1 "+ nome1);
+        }if((numAleatorioJog1 == 0) && (numAleatorioJog2 == 1)) {
+            resultado.setText("O vencedor é jogador 1" + nome1);
+        }if((numAleatorioJog1 == 1) && (numAleatorioJog2 == 2)){
+            resultado.setText("O vencedor é o jogador 1. Parabéns "+ nome1);
+        }
+
+        //Jogador 2
+        //(numAleatorioJog1 == 0) && (numAleatorioJog2 == 1)) tesoura ganha do papel
+        //((numAleatorioJog1 == 1) && (numAleatorioJog2 == 0)) papel ganha da pedra
+        //((numAleatorioJog1 == 2) && (numAleatorioJog2 == 1) pedra ganha da tesoura
+
+
+        if((numAleatorioJog1 == 0) && (numAleatorioJog2 == 2)){
+            resultado.setText("O vencedor é jogador 2. Parabéns " + nome2);
+        }if((numAleatorioJog1 == 1) && (numAleatorioJog2 == 0)) {
+            resultado.setText("O vencedor é jogador 2. Parabéns " +nome2);
+        }if((numAleatorioJog1 == 2) && (numAleatorioJog2 == 1)){
+            resultado.setText("O vencedor é o jogador 2. Parabéns " + nome2);
         }
     }
 }
